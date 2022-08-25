4729bbc1089aac69724e2d2f4ef34c75eb7909a8
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 09:51:43 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String Age = JOptionPane.showInputDialog("Age");
+boolean drivingUnderAge;
+drivingUnderAge = false;
+boolean res1 = 18 == 15;
+System.out.println("res1:" + res1);
+        int value1 = 18;
+int value2 = 15;
+boolean drivingUnderage;
+drivingUnderage = true;
+boolean res2 = value1 > value2;
+System.out.println("res2:" + res2);
+
+
+
+    }
+}
